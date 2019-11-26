d769b15bddc839ae6eaa60601e29adc9489412c2
asmekal <andrey.zharkov@phystech.edu>
2019-11-26 11:20:19 +0300

    data & seminar notebook
    unet & catalyst
    
    changes to barcode dataset
    
    rm dataset

--- week5-segmentation/scripts/runs.sh
+++ week5-segmentation/scripts/runs.sh
@@ -1,3 +1,3 @@
-catalyst-dl run -C segmentation/configs/config_bce_loss.yml --logdir ./logs/bce_loss
-catalyst-dl run -C segmentation/configs/config_iou_loss.yml --logdir ./logs/iou_loss
-catalyst-dl run -C segmentation/configs/config_dice_loss.yml --logdir ./logs/dice_loss
+catalyst-dl run -C segmentation/configs/barcode/config_bce_loss.yml --logdir ./logs/barcode/bce_loss
+catalyst-dl run -C segmentation/configs/barcode/config_iou_loss.yml --logdir ./logs/barcode/iou_loss
+catalyst-dl run -C segmentation/configs/barcode/config_dice_loss.yml --logdir ./logs/barcode/dice_loss
--- week5-segmentation/segmentation/__init__.py
+++ week5-segmentation/segmentation/__init__.py
@@ -1,8 +1,8 @@
 from catalyst.dl import registry
 
 # add experiment
-# from .experiment import BarcodeExperiment as Experiment
-from .experiment import VocExperiment as Experiment
+from .experiment import BarcodeExperiment as Experiment
+# from .experiment import VocExperiment as Experiment
 from .runner import ModelRunner as Runner
 from . import callbacks
 
